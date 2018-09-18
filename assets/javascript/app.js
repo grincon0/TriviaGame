673c522554cb5568abc709c01e0517ea82778916
Wake <georgerincon2@gmail.com>
2018-09-18 18:37:05 -0400

    cleaned up intervals and game progression

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,14 @@
 var wins = 0;
 var losses = 0;
 var index = 0;
-var questionTime = 15;
+////var questionTime = 15;
 var resultsTime = 5;
 var clearThese = ["#time","#question", "#feedback", "#answers", "#gif"];
 var isCorrect = false;
 var isWrong = false;
 var isTimeout = false;
 var hasUserAnswered = false;
+
 
 var stages = [{
         place : 1,
@@ -18,13 +19,14 @@ var stages = [{
     },
     {
         place: 2,
-        question: "another question",
-        answer : [4,5,6,7],
-        correctAnswer : "someAnswers",
+        question: "What is the most common bird in the world?",
+        answers : ["The Robin","The Duck","The Hawk","The chicken"],
+        correctAnswer : "The chicken",
         gif: "assets/images/tester.gif"
 
     }
 ]
+var source = stages[index];
 var game = {
     stage: 0,
     init : function () {
@@ -36,100 +38,144 @@ var game = {
         $("#feedback").append(startBtn);
     },
     nextQuestion : function(){
-        questionTime = 15;
-        resultsTime = 5;
-        game.questionInterval();
+        clearDOM();
+        //questionTime = 15;
+        
+        //game.questionInterval();
         var source = stages[index];
         $("#question").text(source.question);
+
+
         source.answers.forEach((answer) => {
-            var list = $('<ul>');
-            var listItem = $('<li>');
+            
+            var listItem = $('<button>');
             $(listItem).attr('data-name', answer)
+                        .attr('id', 'choice')
                        .text(answer);
-            $(list).append(listItem);
-            $('#answers').append(list);
+            //$(list).append(listItem);
+            $('#answers').append(listItem);
         });
         
     },
     checkAnswer : function () {
         clearDOM();
-        game.resultsInterval();
+        resultsTimer.countdown();
+        
         var source = stages[index];
         if(isCorrect){
             $("#feedback").text(`Correct!`);
             $("#gif").html(`<img src=${source.gif}>`);
-            index++;
-            wins++
+            //game.resultsInterval();
+            wins++;
         }else if(isWrong){
-            $("#feedback").text(`Correct!`);
+            $("#feedback").text(`Wrong!`);
             $("#gif").html(`<img src=${source.gif}>`);
-            index++;
-            losses++
+            //game.resultsInterval();
+            losses++;
         }else if(isTimeout){
             $("#feedback").text(`Out of time!`);
             $("#answers").text(`The correct answer is ${source.correctAnswer}`);
             $("#gif").html(`<img src=${source.gif}>`);
-            index++;
-            losses++
+            //game.resultsInterval();
+            losses++;
         }
     },
-    questionInterval : function (){
-        var interval = setInterval(function(){
-            if(questionTime <= 0){
-                isTimeout = true;
-                clearDOM();
-                game.checkAnswer();
-                clearInterval(interval);
-            }
-            $("#time").text(`Time remaining : ${questionTime}`);
-            questionTime--
-        }, 1000)
-    },
-    resultsInterval : function (){
-        var interval = setInterval(function(){
-            if(resultsTime <= 0){
-                clearDOM();
-                game.nextQuestion();
-                
-                clearInterval(interval);
+}
 
+var questionTimer = {
+    time : 15,
+    countdown : function () {
+        var counter = setInterval(function(){
+            $("#time").text(`Seconds left: ${questionTimer.time}`);
+            questionTimer.time--
+            if((questionTimer.time <= 0)){
+                isTimeout = true;
+                clearInterval(counter);
+                game.checkAnswer();
+            }else if(hasUserAnswered){
+                clearInterval(counter);
             }
-            $("#time").text(`Time remaining : ${resultsTime}`);
-            resultsTime--
-        
         }, 1000);
     }
 }
 
+var resultsTimer = {
+    time : 5,
+    countdown : function () {
+        var counter = setInterval(function(){
+            $("#time").text(`Seconds left: ${resultsTimer.time}`);
+            resultsTimer.time--;
+            if((resultsTimer.time === 0)){
+                clearInterval(counter);
+                refresh();
+                game.nextQuestion();
+            }
+        }, 1000);
+    }
+}
+
+function refresh(){
+    index++
+    isCorrect = false;
+    isWrong = false;
+    isTimeout = false;
+    hasUserAnswered = false;
+    questionTimer.time = 15;
+    resultsTimer.time = 5;
+    
+    clearDOM();
+    questionTimer.countdown();
+    
+    
+
+}
+
 $(document).ready(function(){
     game.init();
-    onClick("#start-button");
-    $('<li>').on("click",function(){
+    onClick("#start-button");   
+});
+
+
+$(document).on("click", "#choice", chooseAnswer);
+
+
+function chooseAnswer(){
+   
         var source = stages[index];
-        var userChoice = $(this).attr('data-name');
+        var userChoice = $(this).attr("data-name");
         if(userChoice === source.correctAnswer){
             isCorrect = true;
             hasUserAnswered = true;
+            //clearInterval(game.questionInterval);
             game.checkAnswer();
         }else if(userChoice != source.correctAnswer){
             isWrong = true;
             hasUserAnswered = true;
+            //clearInterval(game.questionInterval);
             game.checkAnswer();
         }
-    });
     
-});
+}
 
-function onClick(btn){
-    $(btn).on("click", function(){
+function onClick(bt){
+    $(bt).on("click", function(){
         clearDOM();
         game.nextQuestion();
+        questionTimer.countdown();
     });
 }
 function clearDOM(){
     clearThese.forEach((id) => {
         $(id).empty();
     });
+}
+function resetBooleans(){
+    isCorrect = false;
+    isWrong = false;
+    isTimeout = false;
+    hasUserAnswered = false;
+    questionTime = 15;
+    clearDOM();
 }
 
 
